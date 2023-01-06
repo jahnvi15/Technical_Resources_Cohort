1ad9c9e5db6c6b8b132aaa8672269963b3731e0d
jahnvi15 <91893464+jahnvi15@users.noreply.github.com>
2023-01-06 19:55:15 +0530

    4and5

---
+++ Linked_List/Kth_Element.cpp
@@ -0,0 +1,39 @@
+// https://leetcode.com/problems/reverse-linked-list/submissions/872677940/
+class ListNode {
+  public:
+    int val;
+  ListNode * next;
+  ListNode(int x) {
+    val = x;
+    next = NULL;
+  }
+};
+
+ListNode* Reverse_LL(ListNode * head){
+    ListNode * prev = NULL;
+    ListNode * curr = head;
+    ListNode * further = head;
+    while(curr != nullptr){
+        further = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = further;
+    }
+    ListNode * temp = head;
+    head = prev;
+    return head;
+}
+int get_Kth_element(ListNode* head, int k){
+    ListNode* slow = head;
+    ListNode* fast = head;
+    for(int i = 0; i < k ; i ++){
+        fast = fast->next;
+    }
+    while(fast){
+        slow = slow->next;
+        fast = fast->next;
+    }
+    return slow->val;
+}
+
+//Approach2  - reverse ll and k element tak chalo 
---
+++ Linked_List/Reverse_LL.cpp
@@ -0,0 +1,26 @@
+// https://leetcode.com/problems/reverse-linked-list/submissions/872677940/
+
+class ListNode {
+  public:
+    int val;
+  ListNode * next;
+  ListNode(int x) {
+    val = x;
+    next = NULL;
+  }
+};
+
+ListNode* Reverse_LL (ListNode* head){
+    ListNode * prev = NULL;
+    ListNode * curr = head;
+    ListNode * further = head;
+    while(curr != nullptr){
+        further = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = further;
+    }
+    ListNode * temp = head;
+    head = prev;
+    return head;
+}
